01de57aa6422e2dd7625f2a3a4b33ec6a10a9484
Yankı Sırtaş <yankisirtas@hotmail.com>
2022-06-03 16:43:33 +0300

    Fix broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
             items: [
               {
                 label: 'Basics',
-                to: '/docs/basics',
+                to: '/docs/basics/congratulations',
               },
             ],
           },
