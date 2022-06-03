28b98ad1c210e4145fc36f84169eb2a4eb70db1e
Yankı Sırtaş <yankisirtas@hotmail.com>
2022-06-03 15:18:04 +0300

    Add basic site metadata

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,7 +120,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `${new Date().getFullYear()}, KorsanDAO`,
       },
       prism: {
         theme: lightCodeTheme,
